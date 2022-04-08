da4ea53cd0642fb809252239d51fdf1f27292aed
Mohammad Abbasi <101105047+verymamad@users.noreply.github.com>
2022-04-09 01:11:25 +0330

    main

---
+++ view/contact.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fa">
+
+<head>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <meta charset="UTF-8">
+    <title>تماس با ما</title>
+</head>
+
+<body>
+    <div class="container">
+        <h1> تماس با ما </h1>
+        <form action="" method="POST">
+            <label for="email"> ایمیل: </label>
+            <br>
+            <input type="email" name="email" id="email">
+            <br>
+            <label for="title"> عنوان: </label>
+            <br>
+            <input type="text" name="title" id="title">
+            <br>
+            <label for="description">متن پیام: </label>
+            <br>
+            <textarea name="description" id="description"></textarea>
+            <br>
+            <button class="btn" type="submit">ارسال</button>
+        </form>
+    </div>
+</body>
+
+</html>
