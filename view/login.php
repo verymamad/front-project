fde1b654c21a8ce489f343949832704d30837ef5
Mohammad Abbasi <101105047+verymamad@users.noreply.github.com>
2022-04-09 18:06:42 +0330

    main

---
+++ view/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fa">
+
+<head>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <meta charset="UTF-8">
+    <title> ورود </title>
+</head>
+
+<body>
+    <div class="container">
+        <h1> ورود به سایت</h1>
+        <form action="" method="POST">
+            <input type="email" name="email" id="email" placeholder="ایمیل">
+            <br>
+            <input type="password" name="password" id="password" placeholder="رمز عبور">
+            <br>
+            <button class="btn" type="submit">ورود</button>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ view/register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fa">
+
+<head>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <meta charset="UTF-8">
+    <title> ثبت نام </title>
+</head>
+
+<body>
+    <div class="container">
+        <h1> ثبت نام در سایت</h1>
+        <form action="" method="POST">
+            <input type="email" name="email" id="email" placeholder="ایمیل">
+            <br>
+            <input type="text" name="username" id="username" placeholder="نام کاربری">
+            <br>
+            <input type="password" name="password" id="password" placeholder="رمز عبور">
+            <br>
+            <button class="btn" type="submit">ثبت نام</button>
+        </form>
+    </div>
+</body>
+
+</html>
