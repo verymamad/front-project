a1d8396429c30118acb2f5341418b1745102e58a
Mohammad Abbasi <mohammadabbasipersonal@gmail.com>
2022-05-02 03:10:25 +0430

    main

---
+++ view/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/7bd119aee0.js" crossorigin="anonymous"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="search">
+    <input class="search-box" type="search" placeholder="search" size="50">
+    <i class="fa-solid fa-bars bar-menu"></i>
+    </div>
+    <article>
+        <div class="card">
+            <div class="pic">
+                <img class="card-img" src="../assets/css/files/pic4.png">
+                <i class="fa-solid fa-play play"></i>
+            </div>
+            <div class="texts">
+                <p class="file-name">File Name</p>
+                <p class="file-author">Author</p>
+            </div>
+            <i class="fa-solid fa-compact-disc add-song"></i>
+            <img class="music-img" src="../assets/css/files/music.png">
+        </div>
+    </article>
+    <article>
+        <div class="card">
+            <div class="pic">
+                <img class="card-img" src="../assets/css/files/pic4.png">
+                <i class="fa-solid fa-play play"></i>
+            </div>
+            <div class="texts">
+                <p class="file-name">File Name</p>
+                <p class="file-author">Author</p>
+            </div>
+            <i class="fa-solid fa-compact-disc add-song"></i>
+            <img class="music-img" src="../assets/css/files/music.png">
+        </div>
+    </article>
+    <article>
+        <div class="card">
+            <div class="pic">
+                <img class="card-img" src="../assets/css/files/pic4.png">
+                <i class="fa-solid fa-play play"></i>
+            </div>
+            <div class="texts">
+                <p class="file-name">File Name</p>
+                <p class="file-author">Author</p>
+            </div>
+            <i class="fa-solid fa-compact-disc add-song"></i>
+            <img class="music-img" src="../assets/css/files/music.png">
+        </div>
+    </article>
+    <article>
+        <div class="card">
+            <div class="pic">
+                <img class="card-img" src="../assets/css/files/pic4.png">
+                <i class="fa-solid fa-play play"></i>
+            </div>
+            <div class="texts">
+                <p class="file-name">File Name</p>
+                <p class="file-author">Author</p>
+            </div>
+            <i class="fa-solid fa-compact-disc add-song"></i>
+            <img class="music-img" src="../assets/css/files/music.png">
+        </div>
+    </article>
+    <article>
+        <div class="card">
+            <div class="pic">
+                <img class="card-img" src="../assets/css/files/pic4.png">
+                <i class="fa-solid fa-play play"></i>
+            </div>
+            <div class="texts">
+                <p class="file-name">File Name</p>
+                <p class="file-author">Author</p>
+            </div>
+            <i class="fa-solid fa-compact-disc add-song"></i>
+            <img class="music-img" src="../assets/css/files/music.png">
+        </div>
+    </article>
+    <article>
+        <div class="card">
+            <div class="pic">
+                <img class="card-img" src="../assets/css/files/pic4.png">
+                <i class="fa-solid fa-play play"></i>
+            </div>
+            <div class="texts">
+                <p class="file-name">File Name</p>
+                <p class="file-author">Author</p>
+            </div>
+            <i class="fa-solid fa-compact-disc add-song"></i>
+            <img class="music-img" src="../assets/css/files/music.png">
+        </div>
+    </article>
+    
+</body>
+</html>
